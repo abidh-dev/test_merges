8f857ec2c1002159430c1d064c4d44e9fe15ba1d
Engineer AbidH <abidali70sb@gmail.com>
2025-11-06 22:37:39 +0100

    Added Subtraction operation

--- math_functions.py
+++ math_functions.py
@@ -4,3 +4,7 @@ num2 = int(input("Enter number 2: "))
 # addition
 sum_result = num1 + num2
 print(f"Sum: {sum_result}")
+
+# subtraction
+diff_result = num1 - num2
+print(f"Difference: {diff_result}")
